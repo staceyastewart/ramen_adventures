a1c57b5a798eca47c2c75aa74e728b2fb3dec1fd
Thomas Lawton <thomaslawton91@gmail.com>
2017-08-03 10:32:06 -0400

    create new branch for work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { sessions: 'registrations_controller' }
-
+  devise_for :users, :controllers => { sessions: 'users/sessions' }
   resources :shops
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
