704d5d497b139b6a3a351fcd87e9a8a5761215d4
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-08-05 02:26:39 -0400

    add home page component

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import RegisterForm from './components/RegisterForm';
 import ShopPostForm from './components/ShopPostForm';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Home />
       </div>
     );
   }
