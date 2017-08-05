23bebd7b125510d5f0fb711d66c9bb0a5889df99
Thomas Lawton <thomaslawton91@gmail.com>
2017-08-05 13:43:25 -0400

    add active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-class ApplicationController < ActionController::API
-    include ActionController::HttpAuthentication::Token::ControllerMethods
+class ApplicationController < ActionController::Base
+
 end
