50127673bbce570c30f24fb6587919eb397998c1
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-08-10 21:39:13 -0400

    adds searches

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,45 @@
 class PostsController < ApplicationController
-    include Response
+  include Response
 
-    before_action :require_login, only: [:create, :update, :destroy]
+  before_action :require_login, only: [:create, :update, :destroy]
 
-    def index
-        @posts = Post.all
-        json_response(@posts)
+  def index
+    @posts = Post.all
+    q = params[:q]
+    @posts_search    = Post.ransack(content_cont: q).result
+    @shops_search = Shop.ransack(name_cont: q).result
+    json_response(@posts)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    json_response(@post)
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    @post.shop = Shop.find(params[:id])
+    if @post.save
+        json_response(@post, :created)
+    else
+        json_response({:errors => @post.errors.full_messages})
     end
+  end
 
-    def show
-        @post = Post.find(params[:id])
-        json_response(@post)
-    end
+  def update
+    @post = Post.update(post_params)
+    head :no_content
+  end
 
-    def create
-        @post = Post.create!(post_params)
-        @post.shop = Shop.find(params[:id])
-        if @post.save
-            json_response(@post, :created)
-        else
-            json_response({:errors => @post.errors.full_messages})
-        end
-    end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
 
-    def update
-        @post = Post.update(post_params)
-        head :no_content
-    end
+  private
 
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-    end
-
-    private
-
-    def post_params
-        json_params = ActionController::Parameters.new( JSON.parse(request.body.read) )
-        return json_params.require(:post).permit(:content, :date, :shops_id)
-    end
+  def post_params
+    json_params = ActionController::Parameters.new( JSON.parse(request.body.read) )
+    return json_params.require(:post).permit(:content, :date, :shops_id)
+  end
 end
