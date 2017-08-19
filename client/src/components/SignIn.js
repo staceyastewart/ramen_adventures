fa35ca78c5046bc2e74753122ad688cd331b11a6
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-08-19 01:36:17 -0400

    position elements on login or register page

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -7,6 +7,32 @@ class SignIn extends Component {
             <div className="signin-page-container">
                 <SideNav />
                 <div className="signin-container">
+                    <div className="signin-register">
+                        <p>Sign In/Register</p>
+                    </div>
+                    <div className="forms-contain">
+                        <div className="returning">
+                            <p>Returning user sign in</p>
+                            <form>
+                                <div className="login-email">
+                                    <label>Email:</label>
+                                    <input type="text"></input>
+                                </div>
+                                <div className="login-password">
+                                    <label>Password:</label>
+                                    <input type="text"></input>
+                                </div>
+                                <input className="login-submit" type="submit"></input>
+                                <p>Forgot your password?</p>
+                            </form>
+                        </div>
+                        <div className="new-user">
+                            <p>New user registration</p>
+                            <label>Email:</label>
+                            <input type="text"></input>
+                            <input type="submit"></input>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
