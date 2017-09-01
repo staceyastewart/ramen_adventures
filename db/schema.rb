90f1d7bced2cfff3e3fd156810ed67c36608d5bd
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-01 12:01:22 -0400

    adds best_of to post params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901155822) do
+ActiveRecord::Schema.define(version: 20170901155953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170901155822) do
     t.datetime "updated_at",              null: false
     t.integer  "shop_id"
     t.text     "photos",     default: [],              array: true
+    t.boolean  "best_of"
     t.index ["shop_id"], name: "index_posts_on_shop_id", using: :btree
   end
 
