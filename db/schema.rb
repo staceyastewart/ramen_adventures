0316950815a97cd1eabf5949eb3366bbc6715815
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-01 11:59:21 -0400

    adds map column to shops table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901153229) do
+ActiveRecord::Schema.define(version: 20170901155822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170901153229) do
     t.boolean  "recommended"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "map"
   end
 
   create_table "users", force: :cascade do |t|
