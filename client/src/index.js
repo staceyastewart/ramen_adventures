d81e2733b616c8a6e14bf3b5f9120f1033f27bd5
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-09-09 19:36:16 -0400

    remove redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import UserReducer from './reducer_users';
-
-
-const rootReducer = combineReducers({
-  users: UserReducer,
-});
-
-export default rootReducer;
