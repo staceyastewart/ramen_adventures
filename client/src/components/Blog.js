dcac216464da4df50ae72505bc6eda7f93f2a6f7
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-09-14 22:17:34 -0400

    render blog post photos on blog page

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react';
 import BestOfNav from './BestOfNav';
 import BlogPost from './BlogPost';
+import axios from 'axios';
 
 class Blog extends Component {
+    constructor() {
+        super();
+        this.state = {
+            photosFirst: [],
+            photosSecond: [],
+            photosThird: []
+        }
+
+        this.getPhotosFirst = this.getPhotosFirst.bind(this);
+        this.getPhotosSecond = this.getPhotosSecond.bind(this);
+        this.getPhotosThird = this.getPhotosThird.bind(this);
+    }
+
+    componentDidMount() {
+        this.getPhotosFirst();
+        this.getPhotosSecond();
+        this.getPhotosThird();
+    }
+
+    getPhotosFirst() {
+        axios.get('/posts')
+        .then((res) => {
+            for (let i = res.data.length-1; i >= res.data.length - 2; i--) {
+                this.setState(prevState => ({
+                    photosFirst: [...prevState.photosFirst, res.data[i].photos[0]]
+                  }));
+            }
+        });
+    }
+
+    getPhotosSecond() {
+        axios.get('/posts')
+        .then((res) => {
+            for (let i = res.data.length-3; i >= res.data.length - 4; i--) {
+                this.setState(prevState => ({
+                    photosSecond: [...prevState.photosSecond, res.data[i].photos[0]]
+                  }));
+            }
+        });
+    }
+
+    getPhotosThird() {
+        axios.get('/posts')
+        .then((res) => {
+            for (let i = res.data.length-5; i >= res.data.length - 6; i--) {
+                this.setState(prevState => ({
+                    photosThird: [...prevState.photosThird, res.data[i].photos[0]]
+                  }));
+            }
+        });
+    }
+
+
     render() {
         return (
             <div className="blog-container">
                 <BestOfNav />
                 <div className="posts-container">
                     <h2>Recent Posts</h2>
-                    <div className="posts">
-                       <BlogPost />
-                       <BlogPost />
-                       <BlogPost />
-                       <BlogPost />
-                       <BlogPost />
+                    <div className="post-container">
+                        <BlogPost   photosFirst={this.state.photosFirst}
+                                    photosSecond={this.state.photosSecond}
+                                    photosThird={this.state.photosThird}/>
                     </div>
                 </div>
             </div>
--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
 class BlogPost extends Component {
+
     render() {
+        const { photosFirst, photosSecond, photosThird } = this.props;
+    
         return (
-            <div className="post">
-                <div className="post-image">
+            <div>
+                <div className="posts">
+                    {photosFirst.map((photo, key) => <img key={key} src={photo} className="post" alt=""/>)}
+                </div>
+                <div className="post-text">
+                </div>
+                <div className="posts">
+                    {photosSecond.map((photo, key) => <img key={key} src={photo} className="post" alt=""/>)}
+                </div>
+                <div className="post-text">
+                </div>
+                <div className="posts">
+                    {photosThird.map((photo, key) => <img key={key} src={photo} className="post" alt=""/>)}
                 </div>
                 <div className="post-text">
                 </div>
             </div>
-            
         );
     }
 }
