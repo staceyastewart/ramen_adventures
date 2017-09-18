3763fdd84de93774c5004b24b62128a262c27a30
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-18 19:31:00 -0400

    renders test email

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -4,6 +4,6 @@ class SignupMailer < ApplicationMailer
   def signup_confirmation(user)
     @user = user
 
-    mail to: user.email, subject: "Ramen Adventures Signup Confirmation"
+    mail to: @user.email, subject: "Ramen Adventures Signup Confirmation"
   end
 end
--- test/mailers/previews/signup_mailer_preview.rb
+++ test/mailers/previews/signup_mailer_preview.rb
@@ -3,7 +3,7 @@ class SignupMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/signup_mailer/signup_confirmation
   def signup_confirmation
-    SignupMailer.signup_confirmation
+    SignupMailer.signup_confirmation(User.first)
   end
 
 end
