6e78f83fbfad962e4c668ce1726a01a8ae5d9082
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-09-17 21:37:25 -0400

    style blog posts

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import BestOfNav from './BestOfNav';
 
 class BlogPost extends Component {
     render() {
         const { contentToDisplay, photoToDisplay, dateToDisplay } = this.props;
         return (
-            <div>
-                <img src={photoToDisplay} alt="" />
-                <div>{moment(dateToDisplay).format('dddd, MMMM Do, YYYY')}</div>
-                <div>{contentToDisplay}</div>
+            <div className="single-post-container">
+                <BestOfNav />
+                <div className="single-blog-post">
+                    <div className="post-date">{moment(dateToDisplay).format('dddd, MMMM Do, YYYY')}</div>
+                    <div className="image-content">
+                        <img className="blog-image" src={photoToDisplay} alt="" />
+                        <div className="blog-post-content">{contentToDisplay}</div>
+                    </div>
+                </div>
             </div>
         );
     }
