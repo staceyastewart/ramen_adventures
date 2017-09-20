4e871185004f211026995c447aac75f3210bc75d
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-20 17:36:22 -0400

    writes email test

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/post_mailer
   def post_mailer
-    UserMailer.post_mailer
+    UserMailer.post_mailer(User.first)
   end
 
 end
