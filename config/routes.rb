7c228ba537b6b2a73f753bcf119a445bf19cd769
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-10-10 16:52:44 -0400

    add get to routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ end
   post "/search" => "search#index"
 
   root to: "root#index"
+  get "*", to: "root#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
