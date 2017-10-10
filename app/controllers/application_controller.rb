0b6edef60bf4e0e4d0bb40100018710c2a766a9f
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-10-10 17:01:09 -0400

    add not found rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
     include Pundit
     protect_from_forgery with: :null_session
+    rescue_from NotFoundException, :with => :not_found
+
+    def not_found
+        respond_to do |format|
+            format.html { render :file => File.join(Rails.root, 'public', 'index.html')}
+            format.json { render :json => { error: "not found" } }
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ end
   post "/search" => "search#index"
 
   root to: "root#index"
-  get "*", to: "root#index"
+  
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
