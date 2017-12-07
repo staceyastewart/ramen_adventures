4f1cec113c7893a29e07907bddf25e383c9e0466
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-12-07 01:58:08 -0500

    add text to store

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -7,8 +7,17 @@ class Store extends Component {
             <div className="store-container">
                 <h1 className="store-header">Store</h1>
                 <div className="store-top-div">
-                    <img className="store-bookcover" src={book} alt="cover of brian's guidebook" />
-                    <img className="store-bookcover" src={book} alt="cover of brian's guidebook" />
+                    <div className="store-book-container">
+                        <img className="store-bookcover" src={book} alt="cover of brian's guidebook" />
+                            <p>Brian's Guide to 50 Great Tokyo Ramen Shops</p>
+                            <p>Buy on Amazon</p>
+                    </div>
+                    <div className="store-book-container">
+                        <img className="store-bookcover" src={book} alt="cover of brian's guidebook" />
+                            <p>Not in Japan?</p>
+                            <p>I'll ship you the book for $25!</p>
+                            <p>Click on the paypal link and enter your information.</p>
+                    </div>
                 </div>
             </div>   
         );
