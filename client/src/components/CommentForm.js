9f5ec49f9b46806fb6ecfba9cfe7c75e48c409b7
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-12-14 20:39:47 -0500

    add prompt to login for comment

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class CommentForm extends Component {
     constructor() {
         super();
         const userId = localStorage.getItem('id');
-        this.state = { userId, comments: [] }
+        this.state = { 
+            userId, 
+            comments: [],
+            renderMessage: false
+        }
         this.submitComment = this.submitComment.bind(this);
         this.getComments = this.getComments.bind(this);
+        this.renderLogInMessage = this.renderLogInMessage.bind(this);
     }
 
     componentDidMount() {
@@ -17,33 +23,56 @@ class CommentForm extends Component {
     getComments() {
         axios.get(`/posts/${this.props.blogId}/comments`)
         .then(response => {
-            console.log(response);
-            this.setState({ comments: response.data })
+            this.setState({ 
+                comments: response.data,
+             });
         })
+    }
+
+    renderLogInMessage() {
+        return (
+            <div className="comment-message">
+                <p>Please log in to leave a comment</p>
+                <Link to="/signin" className="nav-link" id="comment-login-link">
+                    <div className="signup top-links" onClick={this.props.resetRedirect}>
+                        Log In/Sign Up
+                                </div>
+                </Link>
+            </div>
+        )
     }
 
     submitComment(e) { 
         e.preventDefault();
-        axios.post(`/posts/${this.props.blogId}/comments`, { 
-            comment: { 
-                content: e.target.comment.value, 
-                user_id: parseInt(this.state.userId),
-                post_id: this.props.blogId
-            }
-        });
+        if (this.state.userId) { 
+            axios.post(`/posts/${this.props.blogId}/comments`, { 
+                comment: { 
+                    content: e.target.comment.value, 
+                    user_id: parseInt(this.state.userId),
+                    post_id: this.props.blogId
+                }
+            });
+        } else {
+            this.setState({ renderMessage: true });
+        }
     }
    
     render() {
         return (
             <div>
+                {this.state.renderMessage ? this.renderLogInMessage() : null}
                 <h1>Leave a Comment</h1>
                 <form onSubmit={this.submitComment}>
-                    <input type="text" name="comment" />
+                    <textarea type="text" name="comment" />
+                    <input type="submit" />
                 </form>
                 <h2>Comments:</h2> 
                 {this.state.comments ? 
                 this.state.comments.map((comment, i) => {
-                    return <li key={i}>{comment.content}</li>
+                    //allow type conversion to compare string to int
+                    if (this.props.blogId == comment.post_id) {
+                        return <div key={i}>{comment.content}</div>
+                    }
                 }) : null}          
             </div>
         );
