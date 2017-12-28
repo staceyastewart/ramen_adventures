e62ec606ed662048fee4aef5cbf6d21b6de5409a
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-12-27 20:06:33 -0500

    add form to profile page

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 class Comment extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            commenterId: this.props.comment.user_id
+            commenterId: this.props.comment.user_id,
+            firstName: '',
+            lastName: '',
+            timeOfPost: ''
         }
         this.getCommenterName = this.getCommenterName.bind(this);
     }
@@ -18,13 +22,19 @@ class Comment extends Component {
         let id = this.state.commenterId;
         axios.get(`/users/${id}`)
         .then(response => {
-            console.log(response)
+            this.setState({
+                firstName: response.data.first_name,
+                lastName: response.data.last_name,
+                timeOfPost: response.data.created_at
+            })
         })
     }
+
     render() {
-        let { i, comment } = this.props;
+        let { comment } = this.props;
         return (
             <div>
+                <div>{this.state.firstName} {this.state.lastName} {this.state.timeOfPost}</div>
                 <div>{comment.content}</div>
             </div>
         );
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Checkbox } from 'react-icheck';
 
 class Profile extends Component {
     constructor() {
@@ -12,7 +13,7 @@ class Profile extends Component {
     componentDidMount() {
         this.getUserData();
     }
-    
+
     getUserData() {
         const id = this.state.loggedInUser;
         axios.get(`/users/${id}`)
@@ -24,7 +25,61 @@ class Profile extends Component {
     render() {
         return (
             <div>
-                
+                <form onSubmit={(e) => this.props.registerSubmit(e)} className="register-contain">
+                    <div className="create">
+                        <p className="create-profile">Change your password or subscription settings: </p>
+                        <p className="required">*required fields</p>
+                    </div>
+                    <div className="form-field">
+                        <div className="label">
+                            <label>First Name:*</label>
+                        </div>
+                        <div className="input">
+                            <input type="text" name="firstName" className="input-field first-name" />
+                        </div>
+                    </div>
+                    <div className="form-field">
+                        <div className="label">
+                            <label>Last Name:*</label>
+                        </div>
+                        <div className="input">
+                            <input type="text" name="lastName" className="input-field last-name" />
+                        </div>
+                    </div>
+                    <div className="form-field">
+                        <div className="label">
+                            <label>Email:*</label>
+                        </div>
+                        <div className="input">
+                            <input type="email" name="email" defaultValue={this.props.email} className="input-field email" />
+                        </div>
+                    </div>
+                    <div className="form-field">
+                        <div className="label">
+                            <label>Create <br className="register-br-mobile" />Password:* <br />(5-16 characters)</label>
+                        </div>
+                        <div className="input">
+                            <input type="password" name="password" className="input-field password" />
+                        </div>
+                    </div>
+                    <div className="form-field subscribe-wrapper">
+                        <div className="label">
+                            <label>Click here to receive updates</label>
+                        </div>
+                        <div className="input">
+                            <Checkbox
+                                checkboxClass="icheckbox_square-red"
+                                className="checkbox"
+                                increaseArea="20%"
+                                name="subscribed"
+                                defaultChecked={true}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-field submit-contain">
+                        <input type="submit" value="Register" name="register" className="register" />
+                    </div>
+                </form>
             </div>
         );
     }
